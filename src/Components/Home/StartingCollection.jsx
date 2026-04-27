e0b5583b17676480b400d945dfdcc90f32db467d
mdshihab75 <mostakimshihab75@gmail.com>
2026-04-27 23:45:35 +0600

    StartingImage Add Korse

--- src/Components/Home/StartingCollection.jsx
+++ src/Components/Home/StartingCollection.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import Container from '../ui/Container'
+import StartingImage from '../../assets/Images/startingimage.png'
+import StartingImage2 from '../../assets/Images/startingimage2.png'
+import Image from '../Common/Image'
 const StartingCollection = () => {
   return (
-    <div>StartingCollection</div>
+    <section>
+      <Container>
+        <div className='flex'>
+          <div className='w-1/2'>
+            <Image className='ml-auto h-99.5' src={StartingImage} />
+          </div>
+          <div className='w-1/2'>
+            <Image className='h-99.5 ml-auto' src={StartingImage2} />
+          </div>
+        </div>
+      </Container>
+    </section>
   )
 }
 
