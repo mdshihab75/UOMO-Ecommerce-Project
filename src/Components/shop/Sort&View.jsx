13434125bcc78c8d9601cb4a883088028c5bc2d5
mdshihab75 <mostakimshihab75@gmail.com>
2026-06-02 23:58:55 +0600

    Filter Complete Done

--- src/Components/shop/Sort&View.jsx
+++ src/Components/shop/Sort&View.jsx
@@ -3,25 +3,26 @@ import { IoFilterSharp } from "react-icons/io5";
 import Container from '../ui/Container';
 import { IoMdClose } from "react-icons/io";
 import { IoSearchOutline } from "react-icons/io5";
-import { Range } from "react-range";
+import { Range, getTrackBackground} from "react-range";
 const SortAndView = ({ setView }) => {
   let viewProduct = [2, 3, 4]
   const [filterModal, setFilterModal] = useState(false)
   const handleView = (view) => {
     setView(view)
   }
-  let filterRef = useRef ()
-  useEffect (() => {
-    window.addEventListener ("mousedown", function (e){
-      if (e.target.contains(filterRef.current)){
+  let filterRef = useRef()
+  useEffect(() => {
+    window.addEventListener("mousedown", function (e) {
+      if (e.target.contains(filterRef.current)) {
         setFilterModal(false)
       }
       else {
         setFilterModal(true)
       }
     })
-  },[filterModal])
+  }, [filterModal])
 
+  const [values, setValues] = React.useState([50, 100]);
   const colors = [
     "bg-blue-900",
     "bg-yellow-500",
@@ -71,165 +72,192 @@ const SortAndView = ({ setView }) => {
 
       {/*Filter*/}
       <div ref={filterRef}>
-        <div  className='flex gap-2.5 items-center cursor-pointer'>
-        <IoFilterSharp />
-        <h3 className='font-jost font-medium text-sm text-primary-black leading-6'>FILTER</h3>
-      </div>
+        <div className='flex gap-2.5 items-center cursor-pointer'>
+          <IoFilterSharp />
+          <h3 className='font-jost font-medium text-sm text-primary-black leading-6'>FILTER</h3>
+        </div>
 
-      <div className={`fixed top-0 right-0 h-screen overflow-y-auto bg-primary-white z-50 duration-300 overflow-hidden ${filterModal ? "w-105" : "w-0"
-        }`}>
-        <div className='bg-filterbg px-10'>
-          <div className='flex items-center justify-between pt-8.25 pb-6.5'>
-            <h4 className='font-jost font-medium text-base text-primary-black'>FILTER BY</h4>
-            <IoMdClose onClick={() => setFilterModal(false)} size={20} />
+        <div className={`fixed top-0 right-0 h-screen overflow-y-auto bg-primary-white z-50 duration-300 overflow-hidden ${filterModal ? "w-105" : "w-0"
+          }`}>
+          <div className='bg-filterbg px-10'>
+            <div className='flex items-center justify-between pt-8.25 pb-6.5'>
+              <h4 className='font-jost font-medium text-base text-primary-black'>FILTER BY</h4>
+              <IoMdClose onClick={() => setFilterModal(false)} size={20} />
+            </div>
+
+          </div>
+          {/* Product Categories */}
+          <div className='px-10'>
+            <div className="mt-9.5">
+              <div className="flex items-center justify-between">
+                <h3 className="font-jost font-medium text-[18px] text-primary-black">
+                  PRODUCT CATEGORIES
+                </h3>
+                <span className="text-primary-black cursor-pointer">⌃</span>
+              </div>
+
+              <div className="grid grid-cols-2 gap-15.5 gap-y-3 mt-3.25 font-jost font-normal text-sm leading-6 text-primary-black">
+                <p>Dresses</p>
+                <p>Shorts</p>
+                <p>Sweatshirts</p>
+                <p>Swimwear</p>
+                <p>Jackets</p>
+                <p>T-Shirts & Tops</p>
+                <p>Jeans</p>
+                <p>Trousers</p>
+                <p>Men</p>
+                <p>Jumpers & Cardigans</p>
+              </div>
+            </div>
           </div>
 
-        </div>
-        {/* Product Categories */}
-        <div className='px-10'>
-          <div className="mt-9.5">
+          {/* Colors */}
+          <div className="mt-10 px-10">
             <div className="flex items-center justify-between">
-              <h3 className="font-jost font-medium text-[18px] text-primary-black">
-                PRODUCT CATEGORIES
-              </h3>
-              <span className="text-primary-black cursor-pointer">⌃</span>
+              <h3 className="font-jost font-medium text-[18px] text-primary-black">COLOR</h3>
+              <span className="text-primary-black">⌃</span>
             </div>
 
-            <div className="grid grid-cols-2 gap-15.5 gap-y-3 mt-3.25 font-jost font-normal text-sm leading-6 text-primary-black">
-              <p>Dresses</p>
-              <p>Shorts</p>
-              <p>Sweatshirts</p>
-              <p>Swimwear</p>
-              <p>Jackets</p>
-              <p>T-Shirts & Tops</p>
-              <p>Jeans</p>
-              <p>Trousers</p>
-              <p>Men</p>
-              <p>Jumpers & Cardigans</p>
+            <div className="flex flex-wrap gap-6.25 mt-5.75 gap-x-10">
+              {colors.map((color, index) => (
+                <div
+                  key={index}
+                  className={`w-4 h-4 rounded-full cursor-pointer ${color}`}
+                ></div>
+              ))}
             </div>
           </div>
-        </div>
 
-        {/* Colors */}
-        <div className="mt-10 px-10">
-          <div className="flex items-center justify-between">
-            <h3 className="font-jost font-medium text-[18px] text-primary-black">COLOR</h3>
-            <span className="text-primary-black">⌃</span>
+          {/* Sizes */}
+          <div className="mt-10 px-10">
+            <div className="flex items-center justify-between">
+              <h3 className="font-jost font-medium text-[18px] text-primary-black">SIZES</h3>
+
+              <span className="text-primary-black">⌃</span>
+            </div>
+
+            <div className="flex flex-wrap gap-3 mt-5.75">
+              {sizes.map((size, index) => (
+                <button
+                  key={index}
+                  className="w-12 h-10 border text-xs font-medium text-gray-600 hover:bg-black hover:text-white duration-300"
+                >
+                  {size}
+                </button>
+              ))}
+            </div>
           </div>
 
-          <div className="flex flex-wrap gap-6.25 mt-5.75 gap-x-10">
-            {colors.map((color, index) => (
-              <div
-                key={index}
-                className={`w-4 h-4 rounded-full cursor-pointer ${color}`}
-              ></div>
-            ))}
+          {/* Brands */}
+          <div className="mt-10 px-10">
+            <div className="flex items-center justify-between">
+              <h3 className="font-jost font-medium text-[18px] text-primary-black">BRANDS</h3>
+
+              <span className="text-primary-black">⌃</span>
+            </div>
+
+            {/* Search */}
+            <div className="relative mt-4.5">
+              <input
+                type="text"
+                placeholder="Search"
+                className="w-full h-11 border px-4 text-sm outline-none"
+              />
+
+              <IoSearchOutline className="absolute top-1/2 right-4 -translate-y-1/2 text-gray-400 text-lg" />
+            </div>
+
+            {/* Brand List */}
+            <div className="mt-5 space-y-4">
+              {brands?.map((brand, index) => (
+                <div
+                  key={index}
+                  className="flex items-center justify-between text-sm text-gray-600"
+                >
+                  <div className="flex items-center gap-3">
+                    <input type="checkbox" />
+
+                    <p>{brand.name}</p>
+                  </div>
+
+                  <span>{brand.count}</span>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
 
-        {/* Sizes */}
-        <div className="mt-10 px-10">
-          <div className="flex items-center justify-between">
-            <h3 className="font-jost font-medium text-[18px] text-primary-black">SIZES</h3>
+          {/* Price */}
+          <div className="mt-10 px-10">
+            <div className="flex items-center justify-between">
+              <h3 className="font-jost font-medium text-[18px] text-primary-black">PRICE</h3>
 
-            <span className="text-primary-black">⌃</span>
-          </div>
+              <span className="text-gray-500">⌃</span>
+            </div>
 
-          <div className="flex flex-wrap gap-3 mt-5.75">
-            {sizes.map((size, index) => (
-              <button
-                key={index}
-                className="w-12 h-10 border text-xs font-medium text-gray-600 hover:bg-black hover:text-white duration-300"
-              >
-                {size}
-              </button>
-            ))}
-          </div>
-        </div>
-
-        {/* Brands */}
-        <div className="mt-10 px-10">
-          <div className="flex items-center justify-between">
-            <h3 className="font-jost font-medium text-[18px] text-primary-black">BRANDS</h3>
-
-            <span className="text-primary-black">⌃</span>
-          </div>
-
-          {/* Search */}
-          <div className="relative mt-4.5">
-            <input
-              type="text"
-              placeholder="Search"
-              className="w-full h-11 border px-4 text-sm outline-none"
+            {/* Range */}
+            <Range
+              label="Select your value"
+              step={0.1}
+              min={0}
+              max={100}
+              values={values}
+              onChange={(values) => setValues(values)}
+              renderTrack={({ props, children }) => (
+                <div
+                  {...props}
+                  style={{
+                    ...props.style,
+                    height: "6px",
+                    width: "100%",
+                    borderRadius: "20px",
+                    backgroundColor: "#ccc",
+                  }}
+                >
+                  {children}
+                </div>
+              )}
+              renderThumb={({ props }) => (
+                <div
+                  {...props}
+                  key={props.key}
+                  style={{
+                    ...props.style,
+                    height: "18px",
+                    width: "18px",
+                    borderRadius: "50%",
+                    border: "1px solid black",
+                    backgroundColor: "white",
+                  }}
+                />
+              )}
             />
 
-            <IoSearchOutline className="absolute top-1/2 right-4 -translate-y-1/2 text-gray-400 text-lg" />
+            <div className="flex items-center justify-between mt-4">
+              <p className='font-jost font-normal text-sm leading-10 text-secondary'>Min Price: <span className='text-primary-black'> $29</span></p>
+
+              <p className='font-jost font-normal text-sm leading-10 text-secondary'>Max Price: <span className='text-primary-black'> $937</span></p>
+            </div>
           </div>
 
-          {/* Brand List */}
-          <div className="mt-5 space-y-4">
-            {brands?.map((brand, index) => (
-              <div
-                key={index}
-                className="flex items-center justify-between text-sm text-gray-600"
-              >
-                <div className="flex items-center gap-3">
-                  <input type="checkbox" />
+          {/* Tags */}
+          <div className="flex flex-wrap gap-4.25 mt-8 px-10 mb-50">
+            <button className="px-3 py-2 bg-filtertext font-jost font-normal text-[11px] text-primary-black leading-7.5">
+              ✕ <span className='ml-2.5'>BLUES</span>
+            </button>
 
-                  <p>{brand.name}</p>
-                </div>
+            <button className="px-3 py-2 bg-filtertext font-jost font-normal text-[11px] text-primary-black leading-7.5">
+              ✕ <span className='ml-2.5'>MAX PRICE: $493</span>
+            </button>
 
-                <span>{brand.count}</span>
-              </div>
-            ))}
-          </div>
-        </div>
+            <button className="px-3 py-2 bg-filtertext font-jost font-normal text-[11px] text-primary-black leading-7.5">
+              ✕ <span className='ml-2.5'>Zara</span>
+            </button>
 
-        {/* Price */}
-        <div className="mt-10 px-10">
-          <div className="flex items-center justify-between">
-            <h3 className="font-jost font-medium text-[18px] text-primary-black">PRICE</h3>
-
-            <span className="text-gray-500">⌃</span>
-          </div>
-
-          {/* Range */}
-          <div className="mt-6 relative">
-            <div className="w-full h-0.5 bg-gray-300"></div>
-
-            <div className="absolute top-1/2 left-0 w-3 h-3 bg-white border-2 border-black rounded-full -translate-y-1/2"></div>
-
-            <div className="absolute top-1/2 right-16 w-3 h-3 bg-white border-2 border-black rounded-full -translate-y-1/2"></div>
-
-            <div className="absolute top-1/2 left-0 right-16 h-0.5 bg-black -translate-y-1/2"></div>
-          </div>
-
-          <div className="flex items-center justify-between mt-4 text-xs text-gray-500">
-            <p>Min Price: $29</p>
-
-            <p>Max Price: $937</p>
+            <button className="px-3 py-2 bg-filtertext font-jost font-normal text-[11px] text-primary-black leading-7.5">
+              ✕ <span className='ml-2.5'>RESET FILTER</span>
+            </button>
           </div>
         </div>
-
-        {/* Tags */}
-        <div className="flex flex-wrap gap-4.25 mt-8 px-10 mb-50">
-          <button className="px-3 py-2 bg-filtertext font-jost font-normal text-[11px] text-primary-black leading-7.5">
-            ✕ <span className='ml-2.5'>BLUES</span>
-          </button>
-
-          <button className="px-3 py-2 bg-filtertext font-jost font-normal text-[11px] text-primary-black leading-7.5">
-            ✕ <span className='ml-2.5'>MAX PRICE: $493</span>
-          </button>
-
-          <button className="px-3 py-2 bg-filtertext font-jost font-normal text-[11px] text-primary-black leading-7.5">
-            ✕ <span className='ml-2.5'>Zara</span>
-          </button>
-
-          <button className="px-3 py-2 bg-filtertext font-jost font-normal text-[11px] text-primary-black leading-7.5">
-            ✕ <span className='ml-2.5'>RESET FILTER</span>
-          </button>
-        </div>
-      </div>
 
       </div>
     </div>
